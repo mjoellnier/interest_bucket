329ae4f1fbf846c283ebda67370e1b018cfb5e76
mjoellnier <other@max-wuest.de>
2019-11-11 07:52:15 +0100

    Added initial interest field layout

---
+++ src/components/interestField.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/InterestField.css";
+
+const InterestField = () => {
+  return (
+    <div className="interestWrapper">
+      <div className="interestField">
+        <h2>INTEREST</h2>
+      </div>
+    </div>
+  );
+};
+
+export default InterestField;
---
+++ src/pages/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../style/App.css";
+import InterestField from "../components/interestField";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="interests">
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+        <InterestField />
+      </div>
+    </div>
+  );
+}
+
+export default App;
